8a124b909e956dea9bced9689a39c7400c8b407f
Makarov-K <l.suuurf@gmail.com>
2020-02-24 21:42:41 +0300

    lessons 15 + 16: new components & props

---
+++ src/components/Profile/My_posts/My_posts.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from './My_posts.module.css';
+import Post from "./Post/Post";
+
+
+const My_posts = () => {
+    return (
+        <div>
+            <div>
+                <p>my posts:</p>
+                <textarea placeholder="new post"></textarea>
+                <button>Add post</button>
+            </div>
+            <Post message="Hi people!" likes_quantity="15"/>
+            <Post message="This is looks much easier on videos :(" likes_quantity="20"/>
+        </div>
+    );
+}
+
+export default My_posts;
---
+++ src/components/Profile/My_posts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from './Post.module.css';
+
+const Post = (props) => {
+    return (
+        <div className={classes.item}>
+            <img src="https://im0-tub-by.yandex.net/i?id=b5b08e3e39e0bd1863f0bfc14fddd326&n=13"/>
+            <textarea>{props.message}</textarea>
+            <div className={classes.like}>
+                <p>Likes:{props.likes_quantity}</p>
+                <button>Like</button>
+            </div>
+        </div>
+    );
+}
+
+export default Post;
