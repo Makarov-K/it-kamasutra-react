92d5bfbb9510656a127c9d91238415b103772a13
Makarov-K <l.suuurf@gmail.com>
2020-04-20 22:55:48 +0300

    97. Profile info editing

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import style from './ProfileInfo.module.css'
 import Preloader from "../../Common/Preloader/Preloader";
-import needJob from '../../../assets/img/Looking for a job.jpg';
 import defaultPhoto from '../../../assets/img/lent_54846_big_16.jpg';
 import ProfileStatus from "./ProfileStatus";
 import Contacts from "../../Common/Contacts/Contacts";
@@ -10,9 +9,9 @@ import Contacts from "../../Common/Contacts/Contacts";
 const ProfileInfo = (props) => {
 
     const onNewAvatarLoad = (e) => {
-      if(e.target.files.length){
-          props.putNewAvatar(e.target.files[0])
-      }
+        if (e.target.files.length) {
+            props.putNewAvatar(e.target.files[0])
+        }
     };
 
     if (!props.profile) {
@@ -25,20 +24,39 @@ const ProfileInfo = (props) => {
             <div className={style.ava_description}>
                 <div className={style.ava}>
                     <img src={props.profile.photos.large || defaultPhoto}/>
-                    {props.authId === props.profile.userId
-                    && <input type='file' onChange={onNewAvatarLoad}/>
-                    }
                 </div>
-                <div className={style.description}>
+                {props.authId === props.profile.userId &&
+                <div className={style.changeAvatarButton}>
+                    <input type='file' onChange={onNewAvatarLoad}/>
+                </div>
+                }
+                <div className={style.fullName}>
                     <h3>{props.profile.fullName}</h3>
-                    <p>{props.profile.aboutMe}</p>
-                    <ProfileStatus {...props}/>
-                    {props.profile.lookingForAJob && <img src={needJob}/>}
-                    <div className={style.contacts}>
-                        <h4>Me in Internet:</h4>
-                        <Contacts contacts={props.profile.contacts}/>
-                    </div>
                 </div>
+                <div className={style.status}>
+                    <ProfileStatus {...props}/>
+                </div>
+                <div className={style.aboutMe}>
+                    <p><b>About me: </b>{props.profile.aboutMe}</p>
+                </div>
+                <div className={style.lookingForAJob}>
+                    <h4>Looking for a job:
+                        {props.profile.lookingForAJob
+                            ? " Yes!"
+                            : " No"}
+                    </h4>
+                </div>
+                <div className={style.LookingForAJobDescription}>
+                    <h4>Professional skills: {props.profile.lookingForAJobDescription}</h4>
+                </div>
+                <div className={style.contacts}>
+                    <h4>Me in Internet: {<Contacts contacts={props.profile.contacts}/>}</h4>
+                </div>
+                {props.authId === props.profile.userId &&
+                <div>
+                    <button>edit</button>
+                </div>
+                }
             </div>
         </div>
     );
